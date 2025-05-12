332945aa16024edf877285ec629cec45120e575a
MarcosMpz <95544448+MarcosMpz@users.noreply.github.com>
2025-05-12 02:05:00 -0400

    remove

--- app/api/api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.api.routes import auth, users, patient, appointment, department
-
-api_router = APIRouter()
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(patient.router, prefix="/patients", tags=["patients"])
-api_router.include_router(appointment.router, prefix="/appointments", tags=["appointments"])
-api_router.include_router(department.router, prefix="/departments", tags=["departments"])  # <- Este importa
--- app/utils/response.py
+++
@@ -1,8 +0,0 @@
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-
-def success_response(data, status_code=200):
-    return JSONResponse(content=jsonable_encoder(data), status_code=status_code)
-
-def error_response(message, status_code=400):
-    return JSONResponse(content={"error": message}, status_code=status_code)
