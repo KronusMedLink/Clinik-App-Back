4f9d788d79b2b64f94123d604e1ac2900243d845
MarcosMpz <95544448+MarcosMpz@users.noreply.github.com>
2025-05-12 01:09:13 -0400

    add

--- app/utils/init_db.py
+++ app/utils/init_db.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
-from app.crud import crud_user
+from app.crud.user import create_user, get_user_by_email
 from app.schemas.user import UserCreate
 from app.core.security import get_password_hash
 import os
@@ -10,7 +10,7 @@ def init_admin():
     admin_email = os.getenv("ADMIN_EMAIL", "admin@example.com")
     admin_password = os.getenv("ADMIN_PASSWORD", "admin123")
 
-    user = crud_user.get_user_by_email(db, email=admin_email)
+    user = create_user.get_user_by_email(db, email=admin_email)
     if not user:
         admin_user = UserCreate(
             full_name="Admin",
@@ -18,7 +18,7 @@ def init_admin():
             password=admin_password,
             role="admin"
         )
-        crud_user.create_user(db=db, user=admin_user)
+        get_user_by_email.create_user(db=db, user=admin_user)
         print("✅ Admin user created.")
     else:
         print("ℹ️ Admin user already exists.")
